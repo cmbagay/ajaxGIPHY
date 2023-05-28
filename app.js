28ab3b378621d3568f4188ca3f68f40103124558
cmbagay <126985167+cmbagay@users.noreply.github.com>
2023-05-28 15:35:24 -0700

    completed js code

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
-// const form = document.querySelector('giphy-form');
-// const giphySearch = document.querySelector('input[name=search]') ;
+const form = document.querySelector('#giphy-form');
+const input = document.querySelector('#search') ;
 
-// const giph = document.querySelector('#giphs')
-
-// form.addEventListener('submit', function(e) {
-// 	e.preventDefault();
-
-// 	const searchTerm = giphySearch.value;
-
-// 	//make GIF
-// 	const captureGIF = document.createElement('img');
-// 	captureGIF.src = gif.url;
-
-// 	giph.appendChild(captureGIF);
-// })
+const giphs = document.querySelector('#giphs');
+const removeImages = document.querySelector('#rmvBtn')
 
 //completed API getter
 async function giphySearch(search){
 
-		const response = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=5`); //pulls from giphy api; arg of inputable search term/ limits search result to 5 gifs
-		console.log(response); // JSON data preview
+	//pulls from giphy api; arg of inputable search term/ limits search result to 5 gifs
+	const response = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=25`); 
+	console.log('API Data: ', response); // JSON data preview
 
-		const data = response.data.data;
-		for(let gif of data){
-			console.log('GIF Title: ', gif.title); // console log GET value test
-		}
+	//Access API data tests
+	console.log(`Index of Randomly Chosen GIF: ${rdmGIF()}`)
+	console.log(`GIF Image URL: ${response.data.data[rdmGIF()].images.original.url}`);
+	
+	//append imgs
+	const makeImg = document.createElement('img');
+		makeImg.src = response.data.data[rdmGIF()].images.original.url;
+		makeImg.id = 'img';
+
+	giphs.appendChild(makeImg);
+
+	form.reset(); //reset search bar input
 }
 
+function rdmGIF() {
+	return Math.floor(Math.random() * 25); //choose random GIF from API
+}
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault(); 	
+	console.log(`Search Term: ${input.value}`);
+
+	giphySearch(input.value);
+});
+
+removeImages.addEventListener('click', function(e) {
+	e.preventDefault();
+	
+	while (giphs.firstChild){
+		giphs.removeChild(giphs.firstChild);
+	}
+	
+	form.reset();
+})
